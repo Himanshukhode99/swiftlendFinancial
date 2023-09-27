f6af20d7a264d3ba0d43dc8ccab0632fb2805c01
GanpatiSontakke <ganpati1088.sontakke@gmail.com>
2023-09-27 15:11:49 +0530

    template folder ready

--- swift_lendFinancial/src/app/app.module.ts
+++ swift_lendFinancial/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeDashboardComponent } from './entry/home-dashboard/home-dashboard.component';
 import { HomeHeaderComponent } from './templates/home-header/home-header.component';
+import { HomeFooterComponent } from './templates/home-footer/home-footer.component';
+import { AppHeaterComponent } from './templates/app-heater/app-heater.component';
+import { MenuComponent } from './templates/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeDashboardComponent,
-    HomeHeaderComponent
+    HomeHeaderComponent,
+    HomeFooterComponent,
+    AppHeaterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ swift_lendFinancial/src/app/templates/app-heater/app-heater.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-heater',
+  templateUrl: './app-heater.component.html',
+  styleUrls: ['./app-heater.component.css']
+})
+export class AppHeaterComponent {
+
+}
