65aa358ec0eb7a9423faff81f14d88adb02f7e44
PriyaBitake <priyaBitake21@gmail.com>
2023-09-29 15:05:53 +0530

    Changes

--- swift_lendFinancial/src/app/app.module.ts
+++ swift_lendFinancial/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { HomeDashboardComponent } from './entry/home-dashboard/home-dashboard.component';
 import { HomeHeaderComponent } from './templates/home-header/home-header.component';
 import { HomeFooterComponent } from './templates/home-footer/home-footer.component';
-import { AppHeaterComponent } from './templates/app-heater/app-heater.component';
 import { MenuComponent } from './templates/menu/menu.component';
 
 @NgModule({
@@ -15,7 +14,6 @@ import { MenuComponent } from './templates/menu/menu.component';
     HomeDashboardComponent,
     HomeHeaderComponent,
     HomeFooterComponent,
-    AppHeaterComponent,
     MenuComponent
   ],
   imports: [
