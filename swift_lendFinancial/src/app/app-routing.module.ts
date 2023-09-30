b1a1ddc7a4d9da534031f2b196292ef79c18c85b
V-jeetkhadse <jeet.khadse007@gmail.com>
2023-09-30 17:01:02 +0530

    Router link Added

--- swift_lendFinancial/src/app/app-routing.module.ts
+++ swift_lendFinancial/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeDashboardComponent } from './entry/home-dashboard/home-dashboard.component';
 import { GetCreditScoreComponent } from './entry/get-credit-score/get-credit-score.component';
+import { EnquiryComponent } from './entry/enquiry/enquiry.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,9 @@ const routes: Routes = [
   },
   {
     path:"creditscore" , component:GetCreditScoreComponent
+  },
+  {
+    path:"enquiry" , component:EnquiryComponent
   }
 ];
 
