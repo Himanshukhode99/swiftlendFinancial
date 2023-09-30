cbfb96b841eb6459c17de50578ee3fc67530b849
Himanshukhode99 <himanshukhode001@.gmail.com>
2023-09-30 17:18:53 +0530

    Aboutus Router Link Added

--- swift_lendFinancial/src/app/app-routing.module.ts
+++ swift_lendFinancial/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeDashboardComponent } from './entry/home-dashboard/home-dashboard.component';
 import { GetCreditScoreComponent } from './entry/get-credit-score/get-credit-score.component';
 import { EnquiryComponent } from './entry/enquiry/enquiry.component';
+import { AboutUsComponent } from './entry/about-us/about-us.component';
 
 const routes: Routes = [
   {
@@ -12,7 +13,11 @@ const routes: Routes = [
     path:"creditscore" , component:GetCreditScoreComponent
   },
   {
+
     path:"enquiry" , component:EnquiryComponent
+  },
+  {
+    path:'aboutus',component:AboutUsComponent
   }
 ];
 
--- swift_lendFinancial/src/app/entry/enquiry/enquiry.component.ts
+++ swift_lendFinancial/src/app/entry/enquiry/enquiry.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-enquiry',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./enquiry.component.css']
 })
 export class EnquiryComponent {
+  constructor(private fb:FormBuilder,private router:Router){}
+
+  enquiry:FormGroup;
+  
+  
+  ngOnInit(){
+  this.enquiry=this.fb.group({
+    name:[],
+      email:[],
+      age:[],
+      mob:[],
+      address:[],
+      pan:[]
+     
+      
+  })
+}
+  save(){
+    alert("ThankYou For Enquiry");
+    this.router.navigateByUrl("")
+  }
+
+
+
 
 }
