c41a97cc74d528841e2a65d6e85b166e9a449c87
V-jeetkhadse <jeet.khadse007@gmail.com>
2023-10-07 12:50:02 +0530

    Add Employee Added

---
+++ swift_lendFinancial/src/app/model/customer-addressetails copy.ts
@@ -0,0 +1,14 @@
+export class CustomerAddressetails 
+{
+    house_number:number;
+    landmark:string;
+    street_name:string;
+    city_name:string;
+    taluka:string;
+    district:string;
+    state:string;
+    country:string;
+    pin_code:number;
+
+
+}
